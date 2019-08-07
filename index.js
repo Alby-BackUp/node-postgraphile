e9bd199cd6ca950e5bf8bb7631f0ce92b9bb542c
Alberto Viezzi <aviezzi@outlook.com>
2019-08-07 13:57:19 +0200

    Added postgraphile-plugin-nested-mutations

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const { postgraphile } = require("postgraphile");
 const PgSimplifyInflectorPlugin = require("@graphile-contrib/pg-simplify-inflector");
+const PostGraphileNestedMutations = require('postgraphile-plugin-nested-mutations');
 
 const app = express();
 
 app.use(
     postgraphile(process.env.DATABASE_URL, "public", {
         simpleCollections: "only",
-        appendPlugins: [PgSimplifyInflectorPlugin],
+        appendPlugins: [PgSimplifyInflectorPlugin, PostGraphileNestedMutations],
 
         // Optional customisation
         graphileBuildOptions: {
             pgOmitListSuffix: true,
+            nestedMutationsSimpleFieldNames: true
         },
     })
 );
