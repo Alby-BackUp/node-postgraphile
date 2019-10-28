a9a921a9c087f2e739031187e612c60d00d4beef
Alberto Viezzi <aviezzi@outlook.com>
2019-10-28 20:33:03 +0100

    Add CORS.

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const { postgraphile } = require("postgraphile");
 const PgSimplifyInflectorPlugin = require("@graphile-contrib/pg-simplify-inflector");
 const PostGraphileNestedMutations = require('postgraphile-plugin-nested-mutations');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(
     postgraphile(process.env.DATABASE_URL, "public", {
@@ -18,4 +21,4 @@ app.use(
     })
 );
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
