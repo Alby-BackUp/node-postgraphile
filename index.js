4cffefa84fbd30b841193cf2a2f8bdc07f0a88ed
Alberto Viezzi <aviezzi@outlook.com>
2019-10-29 13:55:07 +0100

    Add https server.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+
 const { postgraphile } = require("postgraphile");
 const PgSimplifyInflectorPlugin = require("@graphile-contrib/pg-simplify-inflector");
 const PostGraphileNestedMutations = require('postgraphile-plugin-nested-mutations');
+
+const https = require('https');
 const cors = require('cors');
+const fs = require('fs');
 
 const app = express();
 
 app.use(cors());
+
+const options = {
+  key: fs.readFileSync('tools/certificates/localhost-key.pem'),
+  cert: fs.readFileSync('tools/certificates/localhost.pem')
+};
 
 app.use(
     postgraphile(process.env.DATABASE_URL, "public", {
@@ -21,4 +30,4 @@ app.use(
     })
 );
 
-app.listen(process.env.PORT || 8080);
+https.createServer(options, app).listen(process.env.PORT || 8080);
